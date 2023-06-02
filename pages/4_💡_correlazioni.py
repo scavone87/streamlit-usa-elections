a877ab791370da4de4bfa2caa83a1a9be37a75c3
scavone87 <scavone87@gmail.com>
2023-06-02 18:35:32 +0200

    Bug fix

--- pages/4_💡_correlazioni.py
+++ pages/4_💡_correlazioni.py
@@ -4,9 +4,19 @@ import numpy as np
 import altair as alt
 from PIL import Image
 
+import os
+
 import services as srv
 
-im = Image.open("imgs\\united-states.png")
+
+# Ottenere il percorso del file relativo allo script corrente
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Costruire il percorso del file immagine utilizzando os.path.join
+image_path = os.path.join(current_dir, 'imgs', 'united-states.png')
+
+# Aprire l'immagine utilizzando il percorso corretto
+im = Image.open(image_path)
 st.set_page_config(page_title='Election USA 2016', page_icon=im, layout='centered', initial_sidebar_state='auto')
 
 st.title("Correlazioni")
